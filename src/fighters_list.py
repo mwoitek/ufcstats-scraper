995061200a454e2a069bd7f60138186dca33448b
Marcio Woitek <mwoitekjr@gmail.com>
2023-11-07 12:31:10 -0300

    (feat) Fighters list: Get list's HTML

---
+++ src/fighters_list.py
@@ -0,0 +1,25 @@
+import requests
+from bs4 import BeautifulSoup
+
+BASE_URL = "http://www.ufcstats.com/statistics/fighters"
+
+
+class FightersListScraper:
+    def __init__(self, first_letter: str) -> None:
+        self.first_letter = first_letter
+        self.failed = False
+
+    def get_soup(self) -> BeautifulSoup | None:
+        params = {
+            "char": self.first_letter,
+            "page": "all",
+        }
+        response = requests.get(BASE_URL, params=params)
+
+        if response.status_code != requests.codes["ok"]:
+            self.failed = True
+            return
+
+        html = response.text
+        self.soup = BeautifulSoup(html, "lxml")
+        return self.soup
