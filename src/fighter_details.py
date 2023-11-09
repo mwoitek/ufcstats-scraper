bce63af3323488a1846767ba7794b83e1c56546a
Marcio Woitek <mwoitekjr@gmail.com>
2023-11-08 23:23:34 -0300

    (feat) Fighter details: Scrape page header

--- src/fighter_details.py
+++ src/fighter_details.py
@@ -1,6 +1,10 @@
+import re
+
 import requests
 from bs4 import BeautifulSoup
 from bs4 import Tag
+
+RECORD_PATTERN = r"Record: (?P<wins>\d+)-(?P<losses>\d+)-(?P<draws>\d+)( \((?P<noContests>\d+) NC\))?"
 
 
 class FighterDetailsScraper:
@@ -18,3 +22,34 @@ class FighterDetailsScraper:
         html = response.text
         self.soup = BeautifulSoup(html, "lxml")
         return self.soup
+
+    def scrape_header(self) -> dict[str, str | int] | None:
+        if not hasattr(self, "soup"):
+            return
+
+        data_dict: dict[str, str | int] = {}
+
+        # scrape full name
+        name_span = self.soup.find("span", class_="b-content__title-highlight")
+        if isinstance(name_span, Tag):
+            data_dict["fullName"] = name_span.get_text().strip()
+
+        # scrape nickname
+        nickname_p = self.soup.find("p", class_="b-content__Nickname")
+        if isinstance(nickname_p, Tag):
+            data_dict["nickname"] = nickname_p.get_text().strip()
+            if data_dict["nickname"] == "":
+                del data_dict["nickname"]
+
+        # scrape record
+        record_span = self.soup.find("span", class_="b-content__title-record")
+
+        if isinstance(record_span, Tag):
+            record_str = record_span.get_text().strip()
+            match = re.match(RECORD_PATTERN, record_str, flags=re.IGNORECASE)
+
+            if isinstance(match, re.Match):
+                record_dict = {k: int(v) for k, v in match.groupdict(default="0").items()}
+                data_dict.update(record_dict)
+
+        return data_dict if len(data_dict) > 0 else None
