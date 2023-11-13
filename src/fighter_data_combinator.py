7cf4f93d9c21b63b3652c8b194ff89f0a9f9845d
Marcio Woitek <mwoitekjr@gmail.com>
2023-11-13 18:05:16 -0300

    (feat) Data combinator: Read JSON files

--- src/fighter_data_combinator.py
+++ src/fighter_data_combinator.py
@@ -1,8 +1,15 @@
+import json
+import os
 from dataclasses import dataclass
+from pathlib import Path
 from typing import Optional
 from typing import cast
 
 COMMON_FIELDS = ["nickname", "height", "weight", "stance", "wins", "losses", "draws"]
+
+DATA_DIR = Path(__file__).resolve().parents[1] / "data"
+DATA_DIR_1 = DATA_DIR / "fighters_list"
+DATA_DIR_2 = DATA_DIR / "fighter_details"
 
 
 @dataclass
@@ -19,6 +26,11 @@ class FighterData1:
     weight: Optional[str] = None
     reach: Optional[str] = None
     stance: Optional[str] = None
+
+    @classmethod
+    def from_dict(cls, d: dict) -> Optional["FighterData1"]:
+        # TODO: add validation
+        return cls(**d)
 
 
 @dataclass
@@ -42,6 +54,11 @@ class FighterData2:
     tdAcc: Optional[int] = None
     tdDef: Optional[int] = None
     subAvg: Optional[float] = None
+
+    @classmethod
+    def from_dict(cls, d: dict) -> Optional["FighterData2"]:
+        # TODO: add validation
+        return cls(**d)
 
 
 @dataclass
@@ -67,6 +84,29 @@ class FighterData:
     tdAcc: Optional[int] = None
     tdDef: Optional[int] = None
     subAvg: Optional[float] = None
+
+
+def read_fighter_data(
+    type_: int,
+    first_letter: str,
+) -> list[Optional[FighterData1]] | list[Optional[FighterData2]] | None:
+    if not (type_ == 1 or type_ == 2):
+        return
+
+    if not (first_letter.isalpha() and len(first_letter) == 1):
+        return
+
+    data_dir = DATA_DIR_1 if type_ == 1 else DATA_DIR_2
+    if not (data_dir.exists() and data_dir.is_dir() and os.access(data_dir, os.R_OK)):
+        return
+
+    in_file = data_dir / f"{first_letter}.json"
+    if not (in_file.exists() and in_file.is_file() and os.access(in_file, os.R_OK)):
+        return
+
+    fighter_class = FighterData1 if type_ == 1 else FighterData2
+    with open(in_file, mode="r") as json_file:
+        return json.load(json_file, object_hook=lambda d: fighter_class.from_dict(d))
 
 
 def validate_common_fields(fd1: FighterData1, fd2: FighterData2) -> bool:
