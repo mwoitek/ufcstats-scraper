824ccc6546c814593ae53448df895af9d80451fa
Marcio Woitek <mwoitekjr@gmail.com>
2023-11-16 18:40:31 -0300

    (feat) Events list: Get page HTML

---
+++ src/events_list.py
@@ -0,0 +1,20 @@
+import requests
+from bs4 import BeautifulSoup
+
+
+class EventsListScraper:
+    BASE_URL = "http://www.ufcstats.com/statistics/events/completed"
+
+    def __init__(self) -> None:
+        self.failed = False
+
+    def get_soup(self) -> BeautifulSoup | None:
+        response = requests.get(EventsListScraper.BASE_URL, params={"page": "all"})
+
+        if response.status_code != requests.codes["ok"]:
+            self.failed = True
+            return
+
+        html = response.text
+        self.soup = BeautifulSoup(html, "lxml")
+        return self.soup
