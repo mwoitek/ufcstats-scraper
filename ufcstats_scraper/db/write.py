2cc686be6fac687d893867af8e0c1418edb9f5e2
Marcio Woitek <mwoitekjr@gmail.com>
2023-12-02 20:39:01 -0300

    (fix) DB: Fix circular import

--- ufcstats_scraper/db/write.py
+++ ufcstats_scraper/db/write.py
@@ -1,15 +1,15 @@
 import sqlite3
-
-from pydantic import validate_call
+from typing import TYPE_CHECKING
 
 from ufcstats_scraper.db.common import DB_PATH
 from ufcstats_scraper.db.exceptions import DBNotSetupError
 from ufcstats_scraper.db.setup import is_db_setup
-from ufcstats_scraper.scrapers.events_list import ScrapedRow as ScrapedEvent
+
+if TYPE_CHECKING:
+    from ufcstats_scraper.scrapers.events_list import ScrapedRow as ScrapedEvent
 
 
-@validate_call
-def write_events(events: list[ScrapedEvent]) -> None:
+def write_events(events: list["ScrapedEvent"]) -> None:
     if not is_db_setup():
         raise DBNotSetupError
 
