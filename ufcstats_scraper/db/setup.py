6f99f990f87741e34d46bb7968d2f7dfe400d265
Marcio Woitek <mwoitekjr@gmail.com>
2023-12-02 02:08:19 -0300

    (refactor) DB: Simplify and improve setup
    - Remove unnecessary try-except blocks.
    - Implement verbose mode without using a lot of if's.
    - Improve setup function.

--- ufcstats_scraper/db/setup.py
+++ ufcstats_scraper/db/setup.py
@@ -11,74 +11,81 @@ from ufcstats_scraper.db.common import TABLES
 from ufcstats_scraper.db.common import TableName
 
 
+def no_op(*args, **kwargs) -> None:
+    _ = args, kwargs
+
+
 @validate_call
 def create_table(table: TableName, verbose: bool = False) -> None:
-    if verbose:
-        print(f'Setting up "{table}" table:', end="\n\n")
+    print_func = print if verbose else no_op
+    print_func(f'Setting up "{table}" table:', end="\n\n")
 
     sql_script_path = SQL_SCRIPTS_DIR / f"create_{table}.sql"
-    try:
-        with open(sql_script_path) as sql_file:
-            sql_script = sql_file.read().rstrip()
-    except FileNotFoundError as exc:
-        raise exc
+    with open(sql_script_path) as sql_file:
+        sql_script = sql_file.read().rstrip()
 
-    try:
-        with sqlite3.connect(DB_PATH) as conn:
-            conn.executescript(sql_script)
-    except sqlite3.Error as exc:
-        raise exc
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.executescript(sql_script)
 
-    if verbose:
-        print(sql_script)
+    print_func(sql_script)
 
 
 @validate_call
 def drop_table(table: TableName, verbose: bool = False) -> None:
-    if verbose:
-        print(f'Removing "{table}" table:', end="\n\n")
+    print_func = print if verbose else no_op
+    print_func(f'Removing "{table}" table:', end="\n\n")
 
     sql_script_path = SQL_SCRIPTS_DIR / f"drop_{table}.sql"
-    try:
-        with open(sql_script_path) as sql_file:
-            sql_script = sql_file.read().rstrip()
-    except FileNotFoundError as exc:
-        raise exc
+    with open(sql_script_path) as sql_file:
+        sql_script = sql_file.read().rstrip()
 
-    try:
-        with sqlite3.connect(DB_PATH) as conn:
-            conn.executescript(sql_script)
-    except sqlite3.Error as exc:
-        raise exc
+    with sqlite3.connect(DB_PATH) as conn:
+        conn.executescript(sql_script)
 
-    if verbose:
-        print(sql_script)
+    print_func(sql_script)
+
+
+def is_db_setup() -> bool:
+    if not DB_PATH.exists():
+        return False
+
+    table_names = map(lambda n: f"'{n}'", TABLES)
+    tables_list = "(" + ", ".join(table_names) + ")"
+    query = f"SELECT name FROM sqlite_master WHERE type = 'table' AND name IN {tables_list}"
+
+    with sqlite3.connect(DB_PATH) as conn:
+        cur = conn.cursor()
+        cur.execute(query)
+        results = cur.fetchall()
+
+    return len(results) == len(TABLES)
 
 
 @validate_call
 def setup(verbose: bool = False) -> None:
+    print_func = print if verbose else no_op
+
+    if is_db_setup():
+        print_func("Links database is already setup. Nothing to do.")
+        return
+
     for i, table in enumerate(TABLES, start=1):
-        try:
-            create_table(table, verbose)
-        except (FileNotFoundError, ValidationError, sqlite3.Error) as exc:
-            raise exc
-        if verbose and i < len(TABLES):
-            print()
+        create_table(table, verbose)
+        if i < len(TABLES):
+            print_func()
 
 
 @validate_call
 def reset(verbose: bool = False) -> None:
+    print_func = print if verbose else no_op
+
     if not DB_PATH.exists():
-        if verbose:
-            print("Links database does not exist. Nothing to reset.", end="\n\n")
+        print_func("Links database does not exist. Nothing to reset.", end="\n\n")
         return
+
     for table in TABLES:
-        try:
-            drop_table(table, verbose)
-        except (FileNotFoundError, ValidationError, sqlite3.Error) as exc:
-            raise exc
-        if verbose:
-            print()
+        drop_table(table, verbose)
+        print_func()
 
 
 if __name__ == "__main__":
