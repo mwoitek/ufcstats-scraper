23a764c21fc12b7b69b9eb024167eb6c53fa6325
Marcio Woitek <mwoitekjr@gmail.com>
2023-12-04 20:51:35 -0300

    (fix) DB class: Fix adapter, close method

--- ufcstats_scraper/db/db.py
+++ ufcstats_scraper/db/db.py
@@ -3,14 +3,14 @@ from datetime import datetime
 from typing import Any
 from typing import Self
 
-from pydantic import HttpUrl
+from pydantic import AnyUrl
 
 from ufcstats_scraper.db.common import DB_PATH
 from ufcstats_scraper.db.common import TABLES
 from ufcstats_scraper.db.exceptions import DBNotSetupError
 
 
-def adapt_url(url: HttpUrl) -> str:
+def adapt_url(url: AnyUrl) -> str:
     return str(url)
 
 
@@ -18,7 +18,7 @@ def adapt_datetime(dt: datetime) -> str:
     return dt.isoformat(sep=" ").split(".")[0]
 
 
-sqlite3.register_adapter(HttpUrl, adapt_url)
+sqlite3.register_adapter(AnyUrl, adapt_url)
 sqlite3.register_adapter(datetime, adapt_datetime)
 
 
@@ -47,7 +47,11 @@ class LinksDB:
         self.cur = self.conn.cursor()
 
     def close(self) -> None:
-        self.conn.commit()
+        # In case DB is already closed
+        try:
+            self.conn.commit()
+        except sqlite3.ProgrammingError:
+            pass
         self.conn.close()
 
     def __del__(self) -> None:
