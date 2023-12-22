89da93a8ed09c64aafc7f1546db8bef2296b3491
Marcio Woitek <mwoitekjr@gmail.com>
2023-12-22 10:04:03 -0300

    (feat) Main: Add fighters-list subcommand

--- ufcstats_scraper/main.py
+++ ufcstats_scraper/main.py
@@ -1,10 +1,14 @@
 import argparse
 from sqlite3 import Error as SqliteError
 
+from pydantic import ValidationError
+
 from ufcstats_scraper.common import console
 from ufcstats_scraper.db.exceptions import DBNotSetupError
+from ufcstats_scraper.scrapers.common import DEFAULT_DELAY
 from ufcstats_scraper.scrapers.events_list import scrape_events_list
 from ufcstats_scraper.scrapers.exceptions import ScraperError
+from ufcstats_scraper.scrapers.fighters_list import scrape_fighters_list
 
 # Main argument parser
 main_parser = argparse.ArgumentParser(
@@ -18,7 +22,7 @@ main_parser.add_argument(
     dest="quiet",
     help="suppress output",
 )
-subparsers = main_parser.add_subparsers(title="subcommands", description="valid subcommands")
+subparsers = main_parser.add_subparsers(title="subcommands")
 
 
 # events-list subcommand
@@ -27,14 +31,40 @@ def events_list(args: argparse.Namespace) -> None:
     scrape_events_list()
 
 
-parser_events_list = subparsers.add_parser("events-list", help="scrape the events list")
+parser_events_list = subparsers.add_parser(
+    "events-list",
+    description="Subcommand for scraping the events list",
+    help="scrape the events list",
+)
 parser_events_list.set_defaults(func=events_list)
+
+
+# fighters-list subcommand
+def fighters_list(args: argparse.Namespace) -> None:
+    console.quiet = args.quiet
+    scrape_fighters_list(args.delay)
+
+
+parser_fighters_list = subparsers.add_parser(
+    "fighters-list",
+    description="Subcommand for scraping the fighters list",
+    help="scrape the fighters list",
+)
+parser_fighters_list.add_argument(
+    "-d",
+    "--delay",
+    type=float,
+    default=DEFAULT_DELAY,
+    dest="delay",
+    help="set delay between requests",
+)
+parser_fighters_list.set_defaults(func=fighters_list)
 
 # Parse arguments and run subcommand
 args = main_parser.parse_args()
 try:
     args.func(args)
-except (DBNotSetupError, OSError, ScraperError, SqliteError, ValueError):
+except (DBNotSetupError, OSError, ScraperError, SqliteError, ValidationError, ValueError):
     console.quiet = False
     console.print_exception()
     exit(1)
