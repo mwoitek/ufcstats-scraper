6d77ba8bda92f0a5e26789a13fdc724c0dcb42fb
Marcio Woitek <mwoitekjr@gmail.com>
2023-12-24 06:05:05 -0300

    (refactor) Improve code for dealing with config

--- ufcstats_scraper/config/__init__.py
+++ ufcstats_scraper/config/__init__.py
@@ -1,24 +1,72 @@
 from pathlib import Path
+from tomllib import TOMLDecodeError
 from tomllib import load
 from typing import Any
 from typing import Literal
+from typing import cast
+
+from pydantic import BaseModel
+from pydantic import PositiveFloat
+from pydantic import ValidationError
+from pydantic import ValidationInfo
+from pydantic import ValidatorFunctionWrapHandler
+from pydantic import field_validator
 
 from ufcstats_scraper.db.common import LinkSelection
 
 LevelType = Literal["DEBUG", "INFO", "WARNING", "ERROR", "CRITICAL"]
 
-# Read default config file
-_config_path = Path(__file__).resolve().parent / "default_config.toml"
-with open(_config_path, mode="rb") as toml_file:
-    _config_dict = load(toml_file)
+
+def fix_invalid(
+    value: Any,
+    handler: ValidatorFunctionWrapHandler,
+    info: ValidationInfo,
+) -> Any:
+    try:
+        return handler(value)
+    except ValidationError:
+        context = cast(dict[str, Any], info.context)
+        field_name = cast(str, info.field_name)
+        return context[field_name]
+
+
+class Defaults(BaseModel):
+    delay: PositiveFloat = 1.0
+    select: LinkSelection = "untried"
+
+    _fix_invalid = field_validator("*", mode="wrap")(fix_invalid)
+
+
+class Logger(BaseModel):
+    enabled: bool = False
+    level: LevelType = "DEBUG"
+    single_file: bool = True
+
+    _fix_invalid = field_validator("*", mode="wrap")(fix_invalid)
+
+
+class Config(BaseModel):
+    defaults: Defaults = Defaults()
+    logger: Logger = Logger()
+
+
+def read_toml(file_path: str | Path) -> dict[str, Any]:
+    try:
+        with open(file_path, mode="rb") as toml_file:
+            return load(toml_file)
+    except (FileNotFoundError, TOMLDecodeError):
+        return {}
+
+
+# Read config file
+_raw_config = read_toml(Path.cwd() / "config.toml")
+_config = Config.model_validate(_raw_config, context=Config().model_dump())
 
 # Default values
-_default_values: dict[str, Any] = _config_dict["defaults"]
-default_delay: float = _default_values["delay"]
-default_select: LinkSelection = _default_values["select"]
+default_delay = _config.defaults.delay
+default_select = _config.defaults.select
 
 # Logger config
-_logger_config: dict[str, Any] = _config_dict["logger"]
-logger_enabled: bool = _logger_config["enabled"]
-logger_level: LevelType = _logger_config["level"]
-logger_single_file: bool = _logger_config["single_file"]
+logger_enabled = _config.logger.enabled
+logger_level = _config.logger.level
+logger_single_file = _config.logger.single_file
