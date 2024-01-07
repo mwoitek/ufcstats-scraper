9c5ef99daf2179e5088ed329e1fc082f5cfb809d
Marcio Woitek <mwoitekjr@gmail.com>
2024-01-07 00:23:43 -0300

    (refactor) Scrapers: Re-write validators

--- ufcstats_scraper/scrapers/validators.py
+++ ufcstats_scraper/scrapers/validators.py
@@ -1,7 +1,7 @@
 import re
 from typing import Callable, Literal, Optional, cast
 
-from pydantic import HttpUrl, PositiveInt, ValidationInfo
+from pydantic import HttpUrl, ValidationInfo, ValidatorFunctionWrapHandler
 
 from ufcstats_scraper.scrapers.common import PercRatio
 
@@ -17,27 +17,32 @@ def check_link(type_: Literal["event", "fighter", "fight"]) -> Callable[[HttpUrl
     return validator
 
 
-# TODO: Turn these into wrap validators
-
-
-def fill_height(height: Optional[PositiveInt], info: ValidationInfo) -> Optional[PositiveInt]:
-    if height is not None:
-        return height
-
-    height_str = info.data.get("height_str")
-    if not isinstance(height_str, str):
+def fill_height(height: Optional[str], handler: ValidatorFunctionWrapHandler) -> Optional[int]:
+    if height is None:
         return
-
-    match = re.match(r"(\d{1})' (\d{1,2})\"", height_str)
-    match = cast(re.Match, match)
-
-    feet = int(match.group(1))
-    inches = int(match.group(2))
-
-    height = feet * 12 + inches
-    return height
+    match = re.match(r"(\d{1})' (\d{1,2})\"", height.strip())
+    assert isinstance(match, re.Match)
+    feet, inches = int(match.group(1)), int(match.group(2))
+    return handler(feet * 12 + inches)
 
 
+def fill_weight(weight: Optional[str], handler: ValidatorFunctionWrapHandler) -> Optional[int]:
+    if weight is None:
+        return
+    match = re.match(r"(\d+) lbs[.]", weight.strip())
+    assert isinstance(match, re.Match)
+    return handler(int(match.group(1)))
+
+
+def fill_reach(reach: Optional[str], handler: ValidatorFunctionWrapHandler) -> Optional[int]:
+    if reach is None:
+        return
+    match = re.match(r"(\d+)([.]0)?\"", reach.strip())
+    assert isinstance(match, re.Match)
+    return handler(int(match.group(1)))
+
+
+# TODO: Turn into wrap validator
 def fill_ratio(value: Optional[PercRatio], info: ValidationInfo) -> Optional[PercRatio]:
     if value is not None:
         return value
@@ -54,33 +59,3 @@ def fill_ratio(value: Optional[PercRatio], info: ValidationInfo) -> Optional[Per
     assert 0.0 <= ratio <= 1.0, f"{info.field_name} - invalid ratio: {ratio}"
 
     return ratio
-
-
-def fill_reach(reach: Optional[PositiveInt], info: ValidationInfo) -> Optional[PositiveInt]:
-    if reach is not None:
-        return reach
-
-    reach_str = info.data.get("reach_str")
-    if not isinstance(reach_str, str):
-        return
-
-    match = re.match(r"(\d+)([.]0)?\"", reach_str)
-    match = cast(re.Match, match)
-
-    reach = int(match.group(1))
-    return reach
-
-
-def fill_weight(weight: Optional[PositiveInt], info: ValidationInfo) -> Optional[PositiveInt]:
-    if weight is not None:
-        return weight
-
-    weight_str = info.data.get("weight_str")
-    if not isinstance(weight_str, str):
-        return
-
-    match = re.match(r"(\d+) lbs[.]", weight_str)
-    match = cast(re.Match, match)
-
-    weight = int(match.group(1))
-    return weight
